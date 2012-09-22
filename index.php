6ae84e643da47984ef7f300b9bf08fc3d80b09b0
Gustavo2012 <gustavo_3522@hotmail.com>
2012-09-22 13:16:20 -0500

    maquetando

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+ $names = array("juan","gustavo","ladislao","anthony");
+ 
+ foreach ($names as $key => $value) {
+    echo "<br>".$value;
+}
+?>
